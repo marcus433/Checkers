e46b2d4bfd59a84f2e6ee9c79a4e80ca2f066480
Marcus Ferrario <imarcusferr@gmail.com>
2019-03-10 16:27:00 -0700

    Add Base Game Class

--- src/edu/sjsu/cs/cs151/checkers/App/Game.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Game.java
@@ -1,7 +1,23 @@
 package edu.sjsu.cs.cs151.checkers.App;
 
 public class Game {
+
+	Piece.Color currentPlayerColor;
+	Player[] players;
+	Gameboard gameboard;
+
 	Game() {
-		
+		reset();
+	}
+
+	public void switchPlayerTurn() {
+		currentPlayerColor = currentPlayerColor == Piece.Color.RED ? 
+													Piece.Color.BLACK : Piece.Color.RED;
+	}
+
+	public void reset() {
+		this.currentPlayerColor = Piece.Color.RED;
+		this.players = new Player[2];
+		this.gameboard = new Gameboard();
 	}
 }
--- src/edu/sjsu/cs/cs151/checkers/App/Player.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Player.java
@@ -1,7 +1,20 @@
 package edu.sjsu.cs.cs151.checkers.App;
 
 public class Player {
+
+	public int numWonGames = 0;
+	public Piece.Color color;
+	public int numPieces = 0; // TODO: adjust based on determined board size.
+
 	Player() {
 		
+	}
+
+	public void movePiece(Piece piece, Location location) {
+		//
+	}
+
+	public void removePiece(Piece piece) {
+		//
 	}
 }
