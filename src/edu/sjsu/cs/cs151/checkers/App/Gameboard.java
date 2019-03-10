2c5bb7ba413eeac470a098ca4ae8ae6979dee330
Marcus Ferrario <imarcusferr@gmail.com>
2019-03-10 16:32:20 -0700

    Gameboard base class

--- src/edu/sjsu/cs/cs151/checkers/App/Gameboard.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Gameboard.java
@@ -1,7 +1,24 @@
 package edu.sjsu.cs.cs151.checkers.App;
 
 public class Gameboard {
+
+	public Size gridSize = Size(8, 8); // Unit is blocks
+	public Piece[] pieces;
+	public Piece currentPiece;
+
 	Gameboard() {
-		
+		this.pieces = new Piece[gridSize.wiidth * gridSize.height];
+	}
+
+	public void selectPiece(Location location) {
+		//
+	}
+
+	public Bool isValidMove(Location destination) {
+		return true;
+	}
+
+	public void movePiece(Location location) {
+		this.currentPiece.location = location; // TODO: use setter & getter so we can add layout hooks.
 	}
 }
