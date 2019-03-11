b30b2253ed02009a572858e1c7f1d1d00c77dc66
Marcus Ferrario <imarcusferr@gmail.com>
2019-03-10 17:45:55 -0700

    Add Base Class Tests

--- src/edu/sjsu/cs/cs151/checkers/App/Player.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Player.java
@@ -2,6 +2,8 @@ package edu.sjsu.cs.cs151.checkers.App;
 
 public class Player {
 
+	private Gameboard gameboard; // gameboard reference. We may implement this with a protocol delegate later.
+
 	public int numWonGames = 0;
 	public Piece.Color color;
 	public int numPieces = 0; // TODO: adjust based on determined board size.
