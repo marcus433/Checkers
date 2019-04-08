b115cf3a9452a2ed3dd291d8301bba56e857a0e7
seanz <zurcher2012sean@gmail.com>
2019-04-07 23:39:13 -0700

    Zurcher 4/7/19 - Added comments for Game.java.

--- src/edu/sjsu/cs/cs151/checkers/App/Game.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Game.java
@@ -1,14 +1,25 @@
 package edu.sjsu.cs.cs151.checkers.App;
 
+/**
+ * Game determines the game logic and flow of the current game session.
+ */
 public class Game {
 	Game() {
 		reset();
 	}
 
+	/**
+	 * switchPlayerTurn allows the other player to interact with their corresponding pieces 
+	 * by flipping currentColor from RED to BLACK, or vice versa.
+	 */
 	public void switchPlayerTurn() {
 		currentColor = currentColor == Piece.Color.RED ? Piece.Color.BLACK : Piece.Color.RED;
 	}
 
+	/**
+	 * reset alters the game and returns it to its initial state
+	 * by resetting the number of pieces, their positions, and setting BLACK to go first.
+	 */
 	public void reset() {
 		currentColor = Piece.Color.BLACK;
 		board = new Board(this);
@@ -16,6 +27,13 @@ public class Game {
 		blackCount = Board.PIECES_PER_SIDE;
 	}
 
+	/**
+	 * checkForWinner checks if either player has reached an end game state
+	 * by checking the number of remaining pieces of each color; if one player is out of
+	 * pieces, then the other player wins.
+	 * 
+	 * This iteration of checkForWinner simply prints a win message.
+	 */
 	public void checkForWinner() {
 		if (this.redCount == 0)
 			System.out.println("Black player won");
@@ -25,10 +43,18 @@ public class Game {
 	
 	// Getters and Setters	
 
+	/**
+	 * getCurrentColor returns which color is currently active and interactable.
+	 * 
+	 * @return - enum RED or BLACK, depending on which turn it is
+	 */
 	public Piece.Color getCurrentColor() {
 	   return currentColor;
 	}
 
+	/**
+	 * decrementPiecesByColor reduces the total number of pieces of a particular color.
+	 */
 	public void decrementPiecesByColor(Piece.Color color) {
 		if (color == Piece.Color.RED)
 			redCount--;
