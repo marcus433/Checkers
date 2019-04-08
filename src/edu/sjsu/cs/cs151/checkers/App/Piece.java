9c00acbe0def5d3c6ad33d4572b3a66162416e4f
seanz <zurcher2012sean@gmail.com>
2019-04-07 23:20:56 -0700

    Zurcher 4/7/19 - Added comments for the Piece class.

--- src/edu/sjsu/cs/cs151/checkers/App/Piece.java
+++ src/edu/sjsu/cs/cs151/checkers/App/Piece.java
@@ -1,5 +1,8 @@
 package edu.sjsu.cs.cs151.checkers.App;
 
+/**
+ * Piece represents an individual piece used by a player in a game of checkers.
+ */
 public class Piece {
 
 	public enum Color {
@@ -13,14 +16,26 @@ public class Piece {
 	
 	// Getters and Setters
 
+	/**
+	 * getColor returns the color element of the current piece.
+	 * @return - the color of the current piece, either RED or BLACK.
+	 */
 	public Color getColor() {
 	   return this.color;
 	}
 
+	/**
+	 * isKing returns the king status of the current piece.
+	 * @return - 1 if king, 0 otherwise
+	 */
 	public boolean isKing() {
 		return this.isKing;
 	}
 
+	/**
+	 * makeKing converts the current piece from a pawn into a king.
+	 * Kinged pieces have more movement options, expanded upon in the Board class.
+	 */
 	public void makeKing() {
 		this.isKing = true;
 	}
